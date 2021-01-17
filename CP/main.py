c20e7ef30db0a84ca93e0791a651520fd52b3928
amrlnic <amoriello.nicola97@gmail.com>
2021-01-17 17:25:59 +0100

    changed "duple_rot_sym_model" with "dup_rot_sym_model"

--- CP/main.py
+++ CP/main.py
@@ -1,11 +1,11 @@
 def configuration(parser):
-    parser.add_argument('--model', default='duple_rot_sym_model', help='The model to be used in order to build the problem for the solver')
+    parser.add_argument('--model', default='dup_rot_sym_model', help='The model to be used in order to build the problem for the solver')
     parser.add_argument('--solver', default='chuffed', help='The solver used to carry out the problem')
     parser.add_argument('--optimization', type=int, default=1, help='The optimization level of MiniZinc compiler')
     parser.add_argument('--free-search', nargs='?', default=False, const=True, help='Allow the solver to use Free Search mode')
     parser.add_argument('--random-seed', type=int, default=42, help='The random seed used in the search')
 
-def main(instance, all_solutions=False, model='duple_rot_sym_model', solver='chuffed', optimization=1, free_search=False, random_seed=42):
+def main(instance, all_solutions=False, model='dup_rot_sym_model', solver='chuffed', optimization=1, free_search=False, random_seed=42):
     import minizinc as mz
 
     model = mz.Model(f'CP/src/models/{model}.mzn')
