055e5e06cef92708bd790cb183461526d61d4ac0
ddomen <daniele.domenichelli.5+ddomen@gmail.com>
2021-01-18 16:52:06 +0100

    removed SAT smtlib

--- SAT/main.py
+++ SAT/main.py
@@ -1,14 +1,14 @@
 def configuration(parser):
     parser.add_argument('--model', default='base_model', help='The model to be used in order to build the problem for the solver')
-    parser.add_argument('--smt-lib', nargs='?', default=False, const=True, help='True=Uses the smt2-lib standard language, False=Uses python api of z3')
+    # parser.add_argument('--smt-lib', nargs='?', default=False, const=True, help='True=Uses the smt2-lib standard language, False=Uses python api of z3')
 
 def main(instance, all_solutions=False, smt_lib=False, model='base_model'):
-    import z3
+    # import z3
 
-    if smt_lib:
-        solver = z3.Solver()
-        problem_solver = z3.parse_smt2_file(f'SAT/src/smt2/{model}.smt')
-        solver.add(problem_solver)
+    # if smt_lib:
+    #     solver = z3.Solver()
+    #     problem_solver = z3.parse_smt2_file(f'SAT/src/smt2/{model}.smt')
+    #     solver.add(problem_solver)
 
         # if solver.check() == z3.sat:
         #     model = solver.model()
@@ -23,11 +23,11 @@ def main(instance, all_solutions=False, smt_lib=False, model='base_model'):
         #             for i in range(presents)
         #         ])
         #     )
-    else:
-        import importlib
-        Model = importlib.import_module(f'SAT.src.python.{model}').Model
-        model = Model(instance)
-        model.solve(all_solutions=all_solutions)
+    # else:
+    import importlib
+    Model = importlib.import_module(f'SAT.src.python.{model}').Model
+    model = Model(instance)
+    model.solve(all_solutions=all_solutions)
 
 
     
