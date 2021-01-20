ae2cbf33e06573d2a8c0ad1aac7537bd7cf10465
ddomen <daniele.domenichelli.5+ddomen@gmail.com>
2021-01-20 18:14:55 +0100

    improved statistics

---
+++ result_extractor.py
@@ -0,0 +1,158 @@
+import sys
+
+def find(ls, filter):
+    if callable(filter):
+        for s in ls:
+            if filter(s): return s
+    else:
+        for s in ls:
+            if s['instance'] == filter: return s
+    return None
+
+def extract(target_file):
+    with open(target_file, 'r') as f: source = [ line.replace('\n', '').split(';') for line in f.readlines()[1:] ]
+    return [ { k: v.strip() for k, v in zip(statistics, row) } for row in source ]
+
+def format_time(seconds):
+    millis = int(float(seconds) * 1000)
+    seconds = int(millis / 1000)
+    millis -= seconds * 1000
+    mins = int(seconds / 60)
+    seconds -= mins * 60
+    hours = int(mins / 60)
+    mins -= hours * 60
+    return f'{hours:02d}:{mins:02d}:{seconds:02d}.{millis:03d}'
+
+target_file = 'statistics.csv'
+
+methods = (
+        ('CP', ('base_model', 'sym_model', 'rot_model', 'dup_sym_model', 'rot_sym_model', 'dup_rot_sym_model', 'base_global_model', 'rot_global_model')),
+        ('SMT', ('base_model', 'sym_model', 'rot_model', 'dup_sym_model', 'rot_sym_model', 'dup_rot_sym_model')),
+        ('SAT', ('base_model', 'rot_model')),
+    )
+
+statistics = ('method', 'model', 'instance', 'sat', 'time', 'nodes', 'propagations', 'memory')
+instances = tuple([ f'{i}x{i}' for i in range(8, 41) ] + [ 'rotation_test' ])
+
+if len(sys.argv) > 1:
+    def make_table(method, model):
+        method, model = method.lower(), model.lower()
+        method_ref = find(methods, lambda m: m[0].lower() == method)
+        if not method_ref: raise Exception(f'Method \'{method}\' not found!')
+        if model not in method_ref[1]: raise Exception(f'Model \'{model}\' not found!')
+
+        print(f'=== RUNNING: {method.upper()} - {model} ===\n')
+
+        stats = [ s for s in extract(target_file) if s['method'].lower() == method and s['model'] == model ]
+
+        special_name = ''
+        if 'global' in model:
+            if 'base' in model: special_name = ' Base Model'
+            else: special_name = ' Rotation Model'
+        
+        table = f'''
+\\begin{{center}}
+    \\begin{{tabular}}{{|c|c|r|r|}}
+        \\hline
+        \\multicolumn{{4}}{{|c|}}{{\\textbf{{Results{special_name}}}}} \\\\
+        \\hline
+        \\textbf{{Instance}} & \\textbf{{Time}} & \\textbf{{Nodes}} & \\textbf{{Propagations}} \\\\
+        
+        \\hline
+'''
+
+        print('=== MISSING INSTANCES ===')
+        for instance in instances:
+            stat = find(stats, instance)
+            instance_name = instance.replace('_', '\\_')
+            if stat is None or stat['sat'] != 'true':
+                if stat is None: print(f'{method} - {model} - {instance}')
+                table += f'\t\t{instance_name} & - & - & - \\\\ \\hline\n'
+            else: table += f'\t\t{instance_name} & {format_time(stat["time"])} & {int(stat["nodes"]):,d} & {int(stat["propagations"]):,d} \\\\ \\hline\n'
+
+        table += '''
+    \\end{tabular}
+\\end{center}
+'''
+
+        print('\n=== LATEX TABLE ===\n')
+        output_file = f'report/results/{method}_{model}.tex'
+        print(f'Saved at: {output_file}')
+
+        with open(f'report/results/{method}_{model}.tex', 'w') as f: f.write(table)
+    
+
+    args = [ a.lower() for a in sys.argv[1:] ]
+    targets = []
+    if '*' in args:
+        for method, models in methods:
+            for model in models:
+                targets.append((method, model))
+    else:
+        if 'cp' in args: method = 'cp'
+        elif 'smt' in args: method = 'smt'
+        elif 'sat' in args: method = 'sat'
+        else: raise Exception('Missing method in argument list')
+
+        rot = 'rot' in args or 'r' in args
+        sym = 'sym' in args or 's' in args
+        dup = 'dup' in args or 'd' in args
+        glob = 'global' in args or 'g' in args
+
+        model = 'model'
+
+        if glob: model = 'global_' + model
+        if sym: model = 'sym_' + model
+        if rot: model = 'rot_' + model
+        if dup: model = 'dup_' + model
+        if not sym and not rot and not dup: model = 'base_' + model
+        targets.append((method, model))
+    
+    for target_method, target_model in targets: make_table(target_method, target_model)
+else:
+    stats = extract(target_file)
+
+    missing = []
+    ordered = [ statistics ]
+    recon = []
+    unrec = []
+    already = []
+    duplicated = []
+
+    total = 0
+    for method, models in methods:
+        for model in models:
+            for instance in instances:
+                total += 1
+                stat = find(stats, lambda s: s['method'] == method and s['model'] == model and s['instance'] == instance)
+                if stat is None:
+                    print(f'Missing instance: {method} - {model} - {instance}')
+                    missing.append((method, model, instance))
+                else:
+                    recon.append((method, model, instance))
+                    ordered.append(tuple(stat.values())) 
+
+    for stat in stats:
+        v = tuple(stat.values())
+        if v[:3] not in already: already.append(v[:3])
+        else:
+            print(f'Duplicated instance: {str.join(" - ", v[:3])}')
+            duplicated.append(v)
+        if v[:3] not in recon:
+            print(f'Unrecognized instance: {str.join(" - ", v[:3])}')
+            unrec.append(v)
+
+    with open(target_file, 'w') as f: f.write(str.join('\n', [ str.join(';', o) for  o in ordered ]))
+
+    def perc(n, cp=None): return f'{len(n):3d}\t|\t{len(n)/(total if cp is None else len(cp)):7.2%}'
+    print(
+f'''
+=== Instances Numbers ===
+Missing:        {perc(missing)}
+Recognized:     {perc(recon, stats)}
+Unrecognized:   {perc(unrec, stats)}
+Duplicated:     {perc(duplicated, stats)}
+Found:          {perc(stats)}
+Total:          {total}
+'''
+    )
--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tqdm
 import multiprocessing 
 from CP.main import main as CP
@@ -33,11 +34,23 @@ def get_execution_time(args):
     return method_name, model, instance_name, result
 
 def main():
+    target_file = 'statistics.csv'
+    stats = ('sat', 'time', 'nodes', 'propagations', 'memory')
     methods = (
         ('CP', CP,  ('base_model', 'sym_model', 'rot_model', 'dup_sym_model', 'rot_sym_model', 'dup_rot_sym_model', 'base_global_model', 'rot_global_model')),
         ('SMT', SMT, ('base_model', 'sym_model', 'rot_model', 'dup_sym_model', 'rot_sym_model', 'dup_rot_sym_model')),
         ('SAT', SAT, ('base_model', 'rot_model')),
     )
+    
+    already_done = []
+    if os.path.exists(target_file):
+        with open(target_file, 'r') as f:
+            for line in (l.replace('\n', '').split(';') for l in f.readlines()[1:]):
+                already_done.append(tuple(line[:3]))
+    else:
+        with open(target_file, 'w') as csv_output: csv_output.write(str.join(";", ('method', 'model', 'instance') + stats) + '\n')
+
+    already_done = tuple(already_done)
     instance_files = os.listdir('instances')
     instances = [ read_instance_from_file(os.path.join('instances', file)) for file in instance_files ]
     trials = [
@@ -45,19 +58,17 @@ def main():
         for method_name, method, models in methods
         for model in models
         for instance, instance_file in zip(instances, instance_files)
+        if (method_name, model, instance_file.replace('.txt', '')) not in already_done
     ]
-    process_num = multiprocessing.cpu_count()
-    print(f'Resolving statistics with {process_num} processes')
+    process_num = int(sys.argv[1]) if len(sys.argv) > 1 else multiprocessing.cpu_count()
+    print(f'Resolving {len(trials)} statistics with {process_num} processes')
 
-    stats = ('sat', 'time', 'nodes', 'propagations', 'memory')
-
-    with open('statistics.csv', 'w') as csv_output:
-        csv_output.write(f'method;model;instance;{str.join(";", stats)}\n')
     
     with multiprocessing.Pool(process_num) as pool, tqdm.tqdm(initial=0, total=len(trials), desc='Solving', unit='trials') as bar:
+        first = '\n'
         for method_name, model, instance_name, result in pool.imap_unordered(get_execution_time, trials):
-            with open('statistics.csv', 'a') as csv_output:
-                csv_output.write(f'{method_name};{model};{instance_name};{str.join(";", [ result[s] for s in stats ])}\n')
+            with open(target_file, 'a') as csv_output: csv_output.write(f'{first}{method_name};{model};{instance_name};{str.join(";", [ result[s] for s in stats ])}\n')
+            first = ''
             bar.update()
 
 if __name__ == '__main__':
